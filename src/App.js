76a69ae67efc3e5cae0e4c903398d1d785be1d5f
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-09 21:57:09 +0100

    Import Components into App.js
    Import the created components thus far into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+// Routing
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Header from './containers/Header';
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from './containers/ProductDetail';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 Not Found!</Route>
+        </>
+      </Router>
     </div>
   );
 }
