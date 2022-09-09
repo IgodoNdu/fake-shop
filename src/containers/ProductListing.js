29db0c6c9553c5477b47b4c51609a0ba7b165517
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-09 22:52:24 +0100

    Fetch from redux store
    Successfully fetched products from the redux store via useSelector

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,7 +1,13 @@
 // Create the Product Listing Component
+//fetch products from the redux store, to access redux store, import: useSelector
+import { useSelector } from "react-redux"; //A hook to access the redux store state
+
 const ProductListing = () => {
+    //fetch products from store
+    const products = useSelector((state) => state);
+    //console.log(products);
     return (
-        <div>
+        <div className="ui grid container">
             <h1>ProductListing</h1>
         </div>
     );
