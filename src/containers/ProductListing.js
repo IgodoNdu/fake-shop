45d7c4504c221e60836a5785e24b5bbf6cf2ce0c
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-09 21:14:30 +0100

    Create the Product Listing Component
    Create a component for the Product Listing

---
+++ src/containers/ProductListing.js
@@ -0,0 +1,10 @@
+// Create the Product Listing Component
+const ProductListing = () => {
+    return (
+        <div>
+            <h1>ProductListing</h1>
+        </div>
+    );
+};
+
+export default ProductListing;
