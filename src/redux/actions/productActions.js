ca3a55cf6a47154ee7fce79cad68e5f4377061c4
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-09 17:36:07 +0100

    setting product actions for the shop
    set up product(s) actions

---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,18 @@
+// set 3 actions for set_product, selected_product, remove_selected_product
+// import the constant 'ActionTypes' for use in setting product actions
+import { ActionTypes } from "../constants/action-types";
+//Note to export product actions for use
+
+export const setProducts = (products) => {
+    return {
+        type: ActionTypes.SET_PRODUCTS,
+        payload: products,
+    };
+};
+
+export const selectedProduct = (product) => {
+    return {
+        type: ActionTypes.SELECTED_PRODUCT,
+        payload: product,
+    };
+};
