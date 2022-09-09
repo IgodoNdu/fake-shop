66a8bcead358182de359f5a9151522211ba43000
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-09 23:18:39 +0100

    Store Content (Products) display
    Implementing the display of store (products), by de-structuring products into it's properties

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,8 +1,22 @@
 // Create the Product Component
+// Also display the store constituents (products)
+import { useSelector } from "react-redux"; //A hook to access the redux store state
+
 const ProductComponent = () => {
+    const products = useSelector((state) => state.allProducts.products);
+    //destructure products
+    const { id, title } = products[0];
     return (
-        <div>
-            <h1>ProductComponent</h1>
+        <div className="four column wide">
+            <div className="ui link cards">
+                <div className="card">
+                    <div className="image">
+                        <div className="content">
+                            <div className="header">{title}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,7 +1,7 @@
 // Create the Product Listing Component
 //fetch products from the redux store, to access redux store, import: useSelector
 import { useSelector } from "react-redux"; //A hook to access the redux store state
-//import product component for the display of store
+//import product component for the display of store (products)
 import ProductComponent from "./ProductComponent"
 
 const ProductListing = () => {
