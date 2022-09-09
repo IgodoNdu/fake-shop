4e942bce27e671d1530ee75e6a158ab3f0656dab
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-09 21:07:47 +0100

    Create Header Component
    Create a component for the header section, then add this component to the App.js file (react app)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './containers/Header';
 
 function App() {
   return (
     <div className="App">
-      <h3>Hello Fake Shop</h3>
+      <Header />
     </div>
   );
 }
