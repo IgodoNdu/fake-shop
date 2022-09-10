45e8d429d38c6be846a94b0e655e5df6191fc54e
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-10 16:53:38 +0100

    Creating the selectedProductReducer
    Created a reducer for the selectedProduct

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -20,4 +20,14 @@ export const productReducer = (state=initialState, { type, payload }) => {
             return state;
     }
 
+};
+
+//create reducer for the selectedProductReducer
+export const selectedProductReducer = (state={}, { type, payload }) => {
+    switch (type) {
+        case ActionTypes.SELECTED_PRODUCT:
+            return {...state, ...payload}     
+        default:
+            return state;
+    }
 }
