50c8825f9b30e1fc39503bbd9465a9292900f8f0
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-10 02:46:07 +0100

    Save fetched products to store
    Having fetched products successfully, we keep the fetched data in our store via dispatch, which calls the setProducts from productActions, and use the productReducer to set/save the fetched product

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -5,14 +5,14 @@ import { useSelector } from "react-redux"; //A hook to access the redux store st
 const ProductComponent = () => {
     const products = useSelector((state) => state.allProducts.products);
     //destructure products
-    const { id, title } = products[0];
+    //const { id, title } = products[0];
     return (
         <div className="four column wide">
             <div className="ui link cards">
                 <div className="card">
                     <div className="image">
                         <div className="content">
-                            <div className="header">{title}</div>
+                            {/* <div className="header">{title}</div> */}
                         </div>
                     </div>
                 </div>
--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,23 +1,27 @@
 // Create the Product Listing Component
 //fetch products from the redux store, to access redux store, import: useSelector
-import { useSelector } from "react-redux"; //A hook to access the redux store state
+import { useDispatch, useSelector } from "react-redux"; //A hook to access the redux store state
 //import product component for the display of store (products)
 import ProductComponent from "./ProductComponent"
 //import axios for API calls (data fetching)
 import axios from "axios";
 import { useEffect } from "react";
+//Having received/fetched the products, there's need to save the data in our store via dispatch
+import { setProducts } from "../redux/actions/productActions";
 
 
 const ProductListing = () => {
     //fetch products from store
     const products = useSelector((state) => state);
+    const dispatch = useDispatch();
 
     //create function to fetch all the products from fakestore API
     const fetchProducts = async () => {
         const response = await axios.get('https://fakestoreapi.com/products').catch((err) => {
             console.log('Err', err);
         });
-        console.log(response.data);
+        //Having received/fetched the products, there's need to save  (set) the data/products in our store via dispatch
+        dispatch(setProducts(response.data));
     };
 
     useEffect(() => {
