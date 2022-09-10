ef836c974b6e98cc83caf8e9100ec2621aa63d93
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-10 15:53:59 +0100

    Make API call for individual product's detail
    Improve the product detail page to make api call for a particular/individual product's details

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -1,5 +1,11 @@
 // Create the Product Detail Component
+//import axios for individual product detail api call
+import axios from "axios";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 const ProductDetail = () => {
+    const { productId } = useParams();
+    console.log(productId);
     return (
         <div>
             <h1>ProductDetail</h1>
