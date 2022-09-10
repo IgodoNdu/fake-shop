5ce788db6cad8d3ddb39c616701e4dd9cd49e762
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-10 01:48:24 +0100

    Fetch all the products
    Import axios to make API call to fakestoreapi.com, to fetch all the products

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -3,11 +3,27 @@
 import { useSelector } from "react-redux"; //A hook to access the redux store state
 //import product component for the display of store (products)
 import ProductComponent from "./ProductComponent"
+//import axios for API calls (data fetching)
+import axios from "axios";
+import { useEffect } from "react";
+
 
 const ProductListing = () => {
     //fetch products from store
     const products = useSelector((state) => state);
-    //console.log(products);
+
+    //create function to fetch all the products from fakestore API
+    const fetchProducts = async () => {
+        const response = await axios.get('https://fakestoreapi.com/products').catch((err) => {
+            console.log('Err', err);
+        });
+        console.log(response.data);
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+    
     return (
         <div className="ui grid container">
             <ProductComponent />
