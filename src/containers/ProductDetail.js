4a9e2c80d3f21d76f743be56648d281730d3bdaa
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-10 16:50:52 +0100

    Update ProductDetail to fetch selected data and dispatch an action
    We made an API call to fetch the details for the selected product, after which we dispatch the selectedProduct action

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -3,9 +3,31 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+//import for dispatch action
+import { useDispatch, useSelector } from "react-redux";
+//import appropriate action for this use-case
+import { selectedProduct } from "../redux/actions/productActions";
+
 const ProductDetail = () => {
     const { productId } = useParams();
-    console.log(productId);
+    //console.log(productId);
+    
+    //for dispatch action: dispatch function reference
+    const dispatch = useDispatch();
+
+    //for accessing the selectedProduct
+    const product = useSelector((state) => state.product);
+    
+    // using the productId, now make API call to FakeStore
+    const  fetchProductDetail = async() => {
+        const response = await axios.get(`https://fakestoreapi.com/products/${productId}`).catch(err =>{
+            console.log("Err", err);
+        });
+
+        //Now let's dispatch the action
+        dispatch(selectedProduct(response.data));
+    };
+
     return (
         <div>
             <h1>ProductDetail</h1>
